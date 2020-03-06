413b10cd1b87d969ed9293cd90b5cca5dc366921
Caleb O'Connell <caleb.oconnell@grandrounds.com>
2020-03-06 15:46:57 -0500

    commiting whatever is not committed yet

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-          <Link to="/">Home</Link>
+          <Link to="/">Home WOAH UPDATED</Link>
           <Link to="/otherpage">Other Page</Link>
         <div>
           <Route exact path="/" component={Fib} />
